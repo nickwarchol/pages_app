dda90fc1564c96d3722874a8ace8b22530ba531d
Nick Warchol <nwarchol123@icloud.com>
2021-05-03 00:00:37 -0700

    Added procfile

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
-from .views import HomePageView, AboutPageView
+from django.contrib import admin
+from django.urls import path, include
 
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home"),
-    path("about/", AboutPageView.as_view(), name="about"),
+    path('/admin', admin.site.urls()),
+    path('', include("pages.urls"))
 ]
